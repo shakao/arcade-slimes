6e02b5188827cc28e4b1505d135b21ca5994e934
shakao <34112083+shakao@users.noreply.github.com>
2020-07-30 10:49:19 -0700

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,19 @@
+namespace slimesSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const floater1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const floater2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const floater3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const floater4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const floor1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const floor2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const ceiling1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const ceiling2 = image.ofBuffer(hex``);
+
+}
